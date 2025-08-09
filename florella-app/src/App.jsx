be9a1e35d0770e9a3e54895e5cbdf6ed27ae8252
Biagiarchiefz <biagiiarchie@gmail.com>
2025-08-09 18:41:16 +0700

    Init Project

---
+++ florella-app/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return <div className="App h-screen bg-blue-400 flex justify-center items-center ">
+    <h1 className="text-white">Init Project</h1>
+  </div>;
+}
+
+export default App;
